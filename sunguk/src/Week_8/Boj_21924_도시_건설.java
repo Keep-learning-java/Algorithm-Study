62b63fd27c679f5ff935b368623f1c2c4300d172
SunGuk <sgk0713@estsoft.com>
2022-06-19 21:11:25 +0900

    [Baekjoon] 21924: 도시 건설

--- sunguk/src/Week_8/Boj_21924_도시_건설.java
+++ sunguk/src/Week_8/Boj_21924_도시_건설.java
@@ -2,9 +2,14 @@ package Week_8;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Boj_21924_도시_건설 {
+
+    private int[] parent;
 
     public void run() {
         try {
@@ -20,65 +25,44 @@ public class Boj_21924_도시_건설 {
 
             while (roadCount-- > 0) {
                 st = new StringTokenizer(br.readLine());
-                int firstBuildingNumber = Integer.parseInt(st.nextToken());
-                int secondBuildingNumber = Integer.parseInt(st.nextToken());
+                int leftNumber = Integer.parseInt(st.nextToken());
+                int rightNumber = Integer.parseInt(st.nextToken());
                 int price = Integer.parseInt(st.nextToken());
                 totalPrice += price;
-                Road road = new Road(firstBuildingNumber, secondBuildingNumber, price);
+                Road road = new Road(leftNumber, rightNumber, price);
                 roads.add(road);
             }
 
             roads.sort(Comparator.naturalOrder());
 
-            Map<Integer, Building> buildingMap = new HashMap<>();
+            parent = new int[buildingCount + 1];
+            for (int i = 0; i < parent.length; i++) {
+                parent[i] = i;
+            }
+
             int selectedRoadCounts = 0;
+
             for (Road road : roads) {
                 if (selectedRoadCounts == buildingCount - 1) break;
 
-                int smallerBuildingNumber = road.getSmallerBuildingNumber();
-                int biggerBuildingNumber = road.getBiggerBuildingNumber();
+                int leftBuildingNumber = road.getLeftNumber();
+                int rightBuildingNumber = road.getRightNumber();
 
-                Building firstBuilding = buildingMap.getOrDefault(smallerBuildingNumber, new Building(smallerBuildingNumber));
-                Building secondBuilding = buildingMap.getOrDefault(biggerBuildingNumber, new Building(biggerBuildingNumber));
-
-                int firstBuildingParentId = getParentBuildingOf(firstBuilding).getId();
-                int secondBuildingParentId = getParentBuildingOf(secondBuilding).getId();
+                int firstBuildingParentId = findParent(leftBuildingNumber);
+                int secondBuildingParentId = findParent(rightBuildingNumber);
 
                 if (firstBuildingParentId != secondBuildingParentId) {
-                    /**
-                     * TODO: 22. 6. 19. building 번호가 낮은 것을 기준으로 부모로 설정하니 동기화시 문제가 됨
-                     * 
-                     * 7 6
-                     * 1 2 1
-                     * 3 4 1
-                     * 5 6 1
-                     * 1 7 2
-                     * 7 3 3
-                     * 5 7 3
-                     *
-                     *    7
-                     *  / | \
-                     * 1  3  5
-                     * |  |  |
-                     * 2  4  6
-                     *
-                     * 1, 3, 5 가 각각이 자신이 최상위 부모로 인지함. 동기화 필요
-                      */
-
-                    syncParent(firstBuilding, secondBuilding);
-                    buildingMap.put(smallerBuildingNumber, firstBuilding);
-                    buildingMap.put(biggerBuildingNumber, secondBuilding);
+                    syncParentsAsSmallerNumber(leftBuildingNumber, rightBuildingNumber);
                     minPrice += road.getPrice();
                     selectedRoadCounts++;
                 }
             }
 
             boolean hasFoundAnswer = true;
-            List<Building> buildings = new ArrayList<>(buildingMap.values());
-            for (int i = 1; i < buildings.size(); i++) {
-                Building firstParent = updateParentBuildingOf(buildings.get(i - 1));
-                Building secondParent = updateParentBuildingOf(buildings.get(i));
-                if (firstParent.getId() != secondParent.getId()) {
+            for (int i = 1; i < buildingCount; i++) {
+                int firstParent = findParent(i);
+                int secondParent = findParent(i + 1);
+                if (firstParent != secondParent) {
                     hasFoundAnswer = false;
                     break;
                 }
@@ -94,85 +78,49 @@ public class Boj_21924_도시_건설 {
         }
     }
 
-    private Building updateParentBuildingOf(Building building) {
-        if (building.hasParent()) {
-            Building parentBuilding = getParentBuildingOf(building.getParentBuilding());
-            building.setParentBuilding(parentBuilding);
-            return parentBuilding;
+    int findParent(int buildingNumber) {
+        if (parent[buildingNumber] != buildingNumber) {
+            parent[buildingNumber] = findParent(parent[buildingNumber]);
         }
-        return building.getParentBuilding();
+        return parent[buildingNumber];
     }
 
-
-    private Building getParentBuildingOf(Building building) {
-        if (building.hasParent()) {
-            return getParentBuildingOf(building.getParentBuilding());
-        }
-        return building.getParentBuilding();
-    }
-
-    private void syncParent(Building firstBuilding, Building secondBuilding) {
-        Building firstBuildingParent = getParentBuildingOf(firstBuilding);
-        Building secondBuildingParent = getParentBuildingOf(secondBuilding);
-        if (firstBuildingParent.getId() < secondBuildingParent.getId()) {
-            firstBuilding.setParentBuilding(secondBuildingParent);
+    void syncParentsAsSmallerNumber(int firstBuildingNumber, int secondBuildingNumber) {
+        int firstParent = findParent(firstBuildingNumber);
+        int secondParent = findParent(secondBuildingNumber);
+        if (firstParent < secondParent) {
+            parent[secondParent] = firstParent;
         } else {
-            secondBuilding.setParentBuilding(firstBuildingParent);
+            parent[firstParent] = secondParent;
         }
     }
 }
 
 class Road implements Comparable<Road> {
-    private final int smallerBuildingNumber;
-    private final int biggerBuildingNumber;
+    private final int leftNumber;
+    private final int rightNumber;
     private final int price;
 
-    public int getSmallerBuildingNumber() {
-        return smallerBuildingNumber;
+    public int getLeftNumber() {
+        return leftNumber;
     }
 
-    public int getBiggerBuildingNumber() {
-        return biggerBuildingNumber;
+    public int getRightNumber() {
+        return rightNumber;
     }
 
     public int getPrice() {
         return price;
     }
 
-    public Road(int firstBuildingNumber, int secondBuildingNumber, int price) {
-        this.smallerBuildingNumber = Math.min(firstBuildingNumber, secondBuildingNumber);
-        this.biggerBuildingNumber = Math.max(firstBuildingNumber, secondBuildingNumber);
+    public Road(int leftBuildingNumber, int rightBuildingNumber, int price) {
+        this.leftNumber = leftBuildingNumber;
+        this.rightNumber = rightBuildingNumber;
         this.price = price;
     }
 
     @Override
     public int compareTo(Road o) { // ascending sort 오름차순
         return price - o.price;
-    }
-}
-
-
-class Building {
-    private final int id;
-    private Building parentBuilding = this;
-
-    public Building(int id) {
-        this.id = id;
-    }
-
-    public Building getParentBuilding() {
-        return parentBuilding;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    boolean hasParent() {
-        return parentBuilding.getId() != id;
-    }
-
-    public void setParentBuilding(Building building) {
-        parentBuilding = building;
     }
 }
