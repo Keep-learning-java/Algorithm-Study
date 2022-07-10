04c601353d5ded6269ffe53cf04b18b22c5eba83
이대직 <30641972+leedjik@users.noreply.github.com>
2022-07-10 12:07:02 +0900

    [Baekjoon]12886: 돌그룹

---
+++ daejik/Week_10/Boj12886.java
@@ -0,0 +1,112 @@
+package Week10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Boj12886 {
+	static Queue<Stones> queue = new LinkedList<Stones>();
+	static boolean[][] visited = new boolean[3][1501];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int cntFirstGroup = Integer.parseInt(st.nextToken());
+		int cntSecondGroup = Integer.parseInt(st.nextToken());
+		int cntThirdGroup = Integer.parseInt(st.nextToken());
+		
+		visited[0][cntFirstGroup] = true;
+		visited[1][cntSecondGroup] = true;
+		visited[2][cntThirdGroup] = true;
+		
+		queue.add(new Stones(cntFirstGroup, cntSecondGroup, cntThirdGroup));
+		
+		while(!queue.isEmpty()) {
+			Stones now = queue.poll();
+			
+			if(sameNumAll(now)) {
+				System.out.println("1");
+				return;
+			}
+			
+			
+			if(now.aGroup != now.bGroup) {
+				Stones next = now;
+				
+				if(now.aGroup > now.bGroup) {
+					next.aGroup -= next.bGroup;
+					next.bGroup += next.bGroup;					
+				} else {
+					next.bGroup -= next.aGroup;
+					next.aGroup += next.aGroup;
+				}
+				
+				if(isNotNegative(next) == true && isVisited(next) == false) {
+					queue.add(next);
+				}
+			}
+			
+			if(now.aGroup != now.cGroup) {
+				Stones next = now;
+				
+				if(now.aGroup > now.cGroup) {
+					next.aGroup -= next.cGroup;
+					next.cGroup += next.cGroup;
+				} else {
+					next.cGroup -= next.aGroup;
+					next.aGroup += next.aGroup;
+				}
+				
+				if(isNotNegative(next) == true && isVisited(next) == false) {
+					queue.add(next);
+				}				
+			}
+			
+			if(now.bGroup != now.cGroup) {
+				Stones next = now;
+				
+				if(now.bGroup > now.cGroup) {
+					next.bGroup -= next.cGroup;
+					next.cGroup += next.cGroup;
+				} else {
+					next.cGroup -= next.bGroup;
+					next.bGroup += next.bGroup;
+				}
+				
+				if(isNotNegative(next) == true && isVisited(next) == false) {
+					queue.add(next);
+				}				
+			}
+		}
+		System.out.println("0");
+
+	}
+	public static boolean isNotNegative(Stones stones) {
+		if(stones.aGroup >= 0 && stones.bGroup >= 0 && stones.cGroup >= 0) return true;
+		return false;
+	}
+	public static boolean sameNumAll(Stones stones) {
+		if(stones.aGroup == stones.bGroup && stones.aGroup == stones.cGroup && stones.bGroup == stones.cGroup) return true;
+		return false;
+	}
+	public static boolean isVisited(Stones stones) {
+		if(visited[0][stones.aGroup] == true && visited[1][stones.bGroup] == true && visited[2][stones.cGroup] == true) return true;
+		visited[0][stones.aGroup] = true;
+		visited[1][stones.bGroup] = true;
+		visited[2][stones.cGroup] = true;
+		return false;
+	}
+
+}
+class Stones{
+	int aGroup, bGroup, cGroup;
+	
+	Stones(int aGroup, int bGroup, int cGroup){
+		this.aGroup = aGroup;
+		this.bGroup = bGroup;
+		this.cGroup = cGroup;
+	}
+}
