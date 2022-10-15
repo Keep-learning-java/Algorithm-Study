f167195104565bb68720816bd08f082fb4c312f6
hyejinhong <hhj732@naver.com>
2022-10-15 16:21:04 +0900

    [LeetCode] 1546: Maximum Number of Non-Overlapping Subarrays With Sum Equals Target_성공

--- hyejin/Week_21/Maximum Number of Non-Overlaping Subarrays With Sum Equals Target.java
+++ hyejin/Week_21/Maximum Number of Non-Overlaping Subarrays With Sum Equals Target.java
@@ -1,62 +1,22 @@
 class Solution {
     public int maxNonOverlapping(int[] nums, int target) {
-        // make prefix
-		int[] prefix = new int[nums.length];
-		prefix[0] = nums[0];
-		for (int i = 1; i < nums.length; i++) {
-			prefix[i] = prefix[i - 1] + nums[i];
-		}
+		HashMap<Integer, Integer> map = new HashMap<>(); // <누적sum, 나온 횟수>
+        map.put(0, 0);
+		int sum = 0;
+		int result = 0;
+		
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
 
-		HashMap<Integer, Integer> map = new HashMap<>(); // <startIndex, endIndex>
-
-		for (int start = 0; start < nums.length; start++) {
-			for (int i = 0; start + i < nums.length; i++) {
-				int end = start + i;
-
-				int sum;
-				if (start == 0)
-					sum = prefix[end];
-				else
-					sum = prefix[end] - prefix[start - 1]; // [start]~[end] 사이 합
-
-				// 합이 같은 경우
-				if (sum == target) {
-					map.put(start, end);
-					check(map, start, end);
-					break;
-				}
+			// 이전에 sum - target이 저장되었었는데
+			// 지금 또 나왔다는 것은
+			// 이 사이의 합이 target 과 같다는 것.
+			if (map.containsKey(sum - target)) {
+				result = Math.max(result, map.get(sum - target) + 1);
 			}
-
+			map.put(sum, result);
 		}
-
-		return map.size(); 
+		
+		return result;
     }
-    public void check(HashMap<Integer, Integer> map, int startIndex, int endIndex) {
-		// 현재 얻은 startIndex ~ endIndex 의 범위가
-		// map에 이미 저장되어있는 범위와 겹치는지 확인
-		
-
-		ArrayList<Integer> removeList = new ArrayList<>();
-		for (int s : map.keySet()) {
-			if (s == startIndex)
-				continue;
-			
-			int e = map.get(s);
-			// 범위 겹침
-			if ((s <= startIndex && e >= endIndex) || e >= startIndex) {
-				// 둘중에, end 가 더 작은 쪽을 살림 (greedy)
-
-				if (e > endIndex) {
-					removeList.add(s);
-				}
-				else {
-					removeList.add(startIndex);
-				}
-			}
-		}
-		
-		for (int remove : removeList) {
-			map.remove(remove);
-		}
-	}
 }
